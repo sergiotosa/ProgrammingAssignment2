04f6f39c408427a0cac72147eccbfb7c1440c1a7
sergiotosa <sergiotosa@gmail.com>
2014-09-20 00:44:30 +0200

    add assignment no2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The aim of this function is to avoid duplications in already calculated values
 
-## Write a short comment describing this function
+## MakeCacheMatrix calculates a "vector" which is a list containing
+## a function to:
+## set inverse of a matrix
+## get the value of the inversed matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+           m <- NULL
+           set <- function(y) {
+                  x <<- y
+                  m <<- NULL
+           }
+           get <- function() x
+           setinv <- function(solve) m <<- solve
+           getinv <- function() m
+           list(set = set, get = get,
+                setinv = setinv,
+                getinv = getinv)
 }
 
-
-## Write a short comment describing this function
+## CacheSolve  calculates the inverse of the matrix if not already calculated
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+          ## Return a matrix that is the inverse of 'x'
+          m <- x$getinv()
+          if(!is.null(m)) {
+                  message("getting cached data")
+                  return(m)
+          }
+          data <- x$get()
+          m <- solve(data, ...)
+          x$setinv(m)
+          m
 }
